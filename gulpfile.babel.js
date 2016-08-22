b3f0762120f32d0b1f8d918a3b8f823d2f91f41c
Jason Lenoble <jason.lenoble@gmail.com>
2016-08-22 11:59:33 +0200

    Revised prepublishing
    Was broken due to the refactoring of Muter into an imported class

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,7 +9,7 @@ import del from 'del';
 import path from 'path';
 
 const originalSrc = gulp.src;
-gulp.src = function() {
+const plumbedSrc = function() {
   return originalSrc.apply(gulp, arguments)
     .pipe(plumber({
       errorHandler: function(err) {
@@ -20,6 +20,7 @@ gulp.src = function() {
 };
 
 const buildDir = 'build';
+const distDir = 'lib';
 const srcGlob = 'src/*.js';
 const testSrcGlob = 'test/*.test.js';
 
@@ -31,49 +32,54 @@ const allBuildGlob = [
 ];
 
 const build = () => {
-  return gulp.src(allSrcGlob, {
+  return plumbedSrc.call(gulp, allSrcGlob, {
     base: process.cwd(),
     since: gulp.lastRun(build)
   })
     .pipe(sourcemaps.init())
     .pipe(babel())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest(buildDir));
 };
 
-const test = () => {
-  return gulp.src(testBuildGlob)
-    .pipe(mocha());
+const testFuncs = new Map();
+const test = (gulpSrc) => {
+  var test = testFuncs.get(gulpSrc);
+  if (!test) {
+    test = () => gulpSrc.call(gulp, testBuildGlob).pipe(mocha());
+    testFuncs.set(gulpSrc, test);
+  }
+  return test;
 };
 
 const watch = (done) => {
   gulp.watch(allSrcGlob, build);
-  gulp.watch(allBuildGlob, test);
+  gulp.watch(allBuildGlob, test(plumbedSrc));
   done();
 };
 
 const dist = () => {
-  return gulp.src(srcGlob)
+  return originalSrc.call(gulp, srcGlob)
     .pipe(jscs())
     .pipe(jscs.reporter())
     .pipe(jscs.reporter('fail'))
     .pipe(babel())
-    .pipe(rename('index.js'))
-    .pipe(gulp.dest('.'));
+    .pipe(gulp.dest(distDir));
 };
 
 const clean = () => {
-  return del(buildDir);
+  return del([buildDir, distDir]);
 };
 
 gulp.task('clean', clean);
 gulp.task('build', build);
-gulp.task('test', gulp.series('build', test));
+gulp.task('test:build', gulp.series('build', test(plumbedSrc)));
+gulp.task('test:dist', gulp.series('build', test(originalSrc)));
 
 gulp.task('watch', watch);
-gulp.task('tdd', gulp.series('test', 'watch'));
+gulp.task('tdd', gulp.series('test:build', 'watch'));
 
 gulp.task('dist', dist);
-gulp.task('prepublish', gulp.series('test', 'clean', 'dist'));
+gulp.task('prepublish', gulp.series('test:dist', 'clean', 'dist'));
 
 gulp.task('default', gulp.parallel('tdd'));
